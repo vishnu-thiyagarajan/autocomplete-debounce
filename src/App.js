8520942f7f2ddff2aa4a78412b9ab0bc0d78f1fa
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2022-01-24 22:18:19 +0530

    debounce auto complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 function App() {
+  const [text, setText] = React.useState('');
+  const [list, setList] = React.useState([])
+  let id;
+  const search = (e) => {
+    const value = e.target.value;
+    setText(value);
+    if (id) clearTimeout(id);
+    const fetchData = () => {
+        fetch('http://localhost:3000/country.json')
+        .then(response => response.json())
+        .then(data => {
+          setList(data.filter((item)=>item.Name.toLowerCase().startsWith(value.toLowerCase())));
+        return;
+      });
+    }
+    return new Promise((resolve) => {
+      id = setTimeout(
+        () => resolve(fetchData()),
+        1000,
+      );
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Autocomplete
+        freeSolo
+        options={list.map((option) => option.Name)}
+        renderInput={(params) => <TextField {...params} label="Country" autoFocus value={text} onChange={search}/>}
+      />
   );
 }
 
